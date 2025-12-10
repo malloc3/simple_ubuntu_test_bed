7062455daab00293aaaf2e3430ae8674c4fd3854
Cannon Mallory <mallorycannon15@gmail.com>
2025-12-09 16:02:03 -0800

    Added a simple run file to run our code

---
+++ test_run_file_1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Hello young coder This is my first run script!"
+echo "I hope I don't break your computer with this!"
+
+MY_VARIABLE_1="This is the first variable just to make sure everythign works proper like"
+echo $MY_VARIABLE_1
+
+echo "==================="
+echo "Okay now lets exacute our git_hellow_world.py script:"
+
+python3 git_hellow_world.py
+
