6778ecb0463d9170fb641236daaa22dc4ebc5576
Cannon Mallory <mallorycannon15@gmail.com>
2025-12-19 14:58:28 -0800

    updadting conda packages and sh script

--- test_run_file_1.sh
+++ test_run_file_1.sh
@@ -2,11 +2,22 @@
 echo "Hello young coder This is my first run script!"
 echo "I hope I don't break your computer with this!"
 
+if conda list --name simple_ubuntu_test_conda_environ >/dev/null 2>&1; then # this should ask if the conda environment exists.   
+    echo "Conda Environment_exists"
+else 
+    echo "Conda environment doesn't exist"
+    echo "creating new conda_environment_from yml text"
+    conda env create -f "test_conda_environ.yml"
+    echo "Conda environment should exit now"
+
+echo "Activating the conda environment"
+conda activate simple_ubuntu_test_conda_environ
+
 MY_VARIABLE_1="This is the first variable just to make sure everythign works proper like"
 echo $MY_VARIABLE_1
 
 echo "==================="
 echo "Okay now lets exacute our git_hellow_world.py script:"
 
-python3 git_hellow_world.py
+conda run -n simple_ubuntu_test_conda_environ python3 git_hellow_world.py
 
