098e932dfde937425d1a2636af1c5d60ad9dee1e
Cannon Mallory <mallorycannon15@gmail.com>
2025-12-19 15:33:44 -0800

    shell script isn't adding conda environment properly but idk

--- test_run_file_1.sh
+++ test_run_file_1.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+yml_file = "test_conda_environ.yml"
+conda_env_name = "simple_ubuntu_test_conda_environ"
+
 echo "Hello young coder This is my first run script!"
 echo "I hope I don't break your computer with this!"
 
-if conda list --name simple_ubuntu_test_conda_environ >/dev/null 2>&1; then # this should ask if the conda environment exists.   
-    echo "Conda Environment_exists"
-else 
-    echo "Conda environment doesn't exist"
-    echo "creating new conda_environment_from yml text"
-    conda env create -f "test_conda_environ.yml"
-    echo "Conda environment should exit now"
+#if [true]
+#then
+#    echo "Conda Environment_exists"
+#else
+#    echo "Conda environment doesn't exist"
+#    echo "creating new conda_environment_from yml text"
+#    #conda env create -f "test_conda_environ.yml"
+#    echo "Conda environment should exit now"
+#fi
 
+echo "creating the conda environment"
+conda env create -f $yml_file --yes
+#conda init 
+echo "running conda init"
+conda init zsh
 echo "Activating the conda environment"
-conda activate simple_ubuntu_test_conda_environ
+conda activate $conda_env_name
+
+conda info | egrep "conda version|active environment"
 
 MY_VARIABLE_1="This is the first variable just to make sure everythign works proper like"
 echo $MY_VARIABLE_1
