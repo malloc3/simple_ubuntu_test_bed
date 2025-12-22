0eceacfbc8ef9716908d3d79490dc9098e6a30f3
Cannon Mallory <mallorycannon15@gmail.com>
2025-12-22 11:51:32 -0800

    Added text to the code to manage conda environment a litle and provide some help to the user.  It is not a great way but it is simple and works

--- test_run_file_1.sh
+++ test_run_file_1.sh
@@ -1,35 +1,23 @@
 #!/bin/bash
-yml_file = "test_conda_environ.yml"
-conda_env_name = "simple_ubuntu_test_conda_environ"
+required_environ_1_yml_file="test_conda_environ.yml"  # This is the name of the yml file that should help build the correct environment
+required_environ_1="simple_ubuntu_test_conda_environ" #this is the name of the environment that is built from the yaml text.
 
-echo "Hello young coder This is my first run script!"
+echo "======================"
+echo "This code requires a conda environment named '$required_environ_1'"
+echo "If you do not have this environment already you can build it from the yml text in '$required_environ_1_yml_file'"
+echo "This yml file should be available in the in my github account - CM.   Good Luck!"
+echo "======================"
+
+
+
+echo "Hello new coder This is my first run script!"
 echo "I hope I don't break your computer with this!"
 
-#if [true]
-#then
-#    echo "Conda Environment_exists"
-#else
-#    echo "Conda environment doesn't exist"
-#    echo "creating new conda_environment_from yml text"
-#    #conda env create -f "test_conda_environ.yml"
-#    echo "Conda environment should exit now"
-#fi
 
-echo "creating the conda environment"
-conda env create -f $yml_file --yes
-#conda init 
-echo "running conda init"
-conda init zsh
-echo "Activating the conda environment"
-conda activate $conda_env_name
 
-conda info | egrep "conda version|active environment"
-
-MY_VARIABLE_1="This is the first variable just to make sure everythign works proper like"
-echo $MY_VARIABLE_1
 
 echo "==================="
 echo "Okay now lets exacute our git_hellow_world.py script:"
 
-conda run -n simple_ubuntu_test_conda_environ python3 git_hellow_world.py
+conda run -n $required_environ_1 python3 git_hellow_world.py
 
